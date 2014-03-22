fafbb3439e355d9629635bab16c4093639fe1d94
Marco Bettiolo <marco@bettiolo.it>
2014-03-22 21:11:25 +0000

    Deployment script work in progress

--- deploy-to-azure.sh
+++ deploy-to-azure.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-: ${1?"Usage: $0 path-to-publishsettings"}
+
+: ${1?"Usage: $0 <path-to-publishsettings>"}
+
+PUBLISHSETTINGS=`pwd`/$1
+echo ${PUBLISHSETTINGS}
 
 echo "Installing Windows Azure command line tools"
-npm install azure-cli --global
+npm install azure-cli --global || { echo "Failed, aborting." ; exit 1; }
 
-echo "Importing Windows Azure account credentials: $1"
-azure account import $1
+echo "Importing Windows Azure account credentials: ${PUBLISHSETTINGS}"
+azure account import ${PUBLISHSETTINGS} || { echo "Failed, aborting." ; exit 1; }
 
-echo "Creating default deployment script for node"
-azure site deploymentscript --node
+# echo "Creating default deployment script for node"
+# azure site deploymentscript --node || { echo "Failed, aborting." ; exit 1; }
 
 CREATED=`date -u +"%Y-%m-%dT%H-%M-%SZ"`
-echo "Creating virtual machine: oauth-console-$CREATED"
-azure site create oauth-console-$CREATED --location "West Europe" --gitusername deploy
+echo "Creating virtual machine: oauth-console-${CREATED}"
+azure site create oauth-console-${CREATED} --location "West Europe" --gitusername deploy || { echo "Failed, aborting." ; exit 1; }
 
 echo "Git pushing to azure origin"
-git push azure master
+git push azure master || { echo "Failed, aborting." ; exit 1; }
 
 echo "Clearing imported Windows Azure account"
-azure account clear
+azure account clear || { echo "Failed, aborting." ; exit 1; }
 
-echo "Deleting Windows Azure account credentials"
+# echo "Deleting Windows Azure account credentials"
