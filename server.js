dacf65a14950b5238c9e2b29604f7389c022f506
Marco Bettiolo <marco@bettiolo.it>
2014-03-22 22:58:10 +0000

    Default port changed to the same one as in Vagrant

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = process.env.port || 10080;
+var port = process.env.port || 8000;
 var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
