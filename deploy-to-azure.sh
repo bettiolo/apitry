83c35db2912d99e15eeb917d9effa283b9a79084
Marco Bettiolo <marco@bettiolo.it>
2014-03-23 02:03:29 +0000

    Improved messages

--- deploy-to-azure.sh
+++ deploy-to-azure.sh
@@ -5,24 +5,24 @@
 PUBLISHSETTINGS=`pwd`/${1}
 GITUSER=${2}
 
-echo "Installing Windows Azure command line tools"
+echo "Installing Azure command line tools"
 npm install azure-cli --global || { echo "Failed, aborting." >&2; exit 1; }
 
-echo "Importing Windows Azure account credentials: ${PUBLISHSETTINGS}"
+echo "Importing Azure account credentials: ${PUBLISHSETTINGS}"
 azure account import ${PUBLISHSETTINGS} || { echo "Failed, aborting." >&2; exit 1; }
 
 CREATED=`date -u +"%Y-%m-%dT%H-%M-%SZ"`
-echo "Creating virtual machine: oauth-console-${CREATED}"
+echo "Creating Azure site: oauth-console-${CREATED}"
 azure site create oauth-console-${CREATED} --location "West Europe" --git --gitusername ${GITUSER} || { echo "Failed, aborting." >&2; exit 1; }
 
-echo "Git pushing to 'azure' remote"
+echo "Pushing to 'azure' remote"
 git push azure master || { echo "Failed, aborting." >&2; exit 1; }
 
 echo "Deleting 'azure' remote"
 git remote remove azure || { echo "Failed, aborting." >&2; exit 1; }
 
-echo "Clearing imported Windows Azure account"
+echo "Clearing imported Azure account"
 azure account clear || { echo "Failed, aborting." >&2; exit 1; }
 
-# echo "Deleting Windows Azure account credentials"
+# echo "Deleting Azure account credentials"
 # rm ${PUBLISHSETTINGS} || { echo "Failed, aborting." >&2; exit 1; }
