ba307aafa1018cbc81308e5dde2bf9de9e1387cd
Marco Bettiolo <marco@bettiolo.it>
2014-03-23 00:03:37 +0000

    Fixed packer folder

--- vagrant-create-update-box.sh
+++ vagrant-create-update-box.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ ! -d "./packer-arch/" ]; then
+if [ ! -d "packer/" ]; then
 	echo "Creating packer/ folder"
 	mkdir packer/ || { echo "Failed, aborting." >&2 ; exit 1; }
 fi
 echo "Changing to packer/"
-cd packer || { echo "Failed, aborting." >&2 ; exit 1; }
+cd packer/ || { echo "Failed, aborting." >&2 ; exit 1; }
 if [ ! -d "./packer-arch/" ]; then
 	echo "Cloning packer-arch"
 	git clone https://github.com/elasticdog/packer-arch.git || { echo "Failed, aborting." >&2 ; exit 1; }
