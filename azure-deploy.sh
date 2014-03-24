0bf09c21146aa2971a5123130b175522b1a6f70c
Marco Bettiolo <marco@bettiolo.it>
2014-03-24 08:59:40 +0000

    Work in progress blue green deploy

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -21,25 +21,28 @@ INSTANCES_RUNNING=0
 if echo "${SITES}" | grep -Eq "^data:\s+oauth-console-GREEN\s+.*Running"
 then
 	echo "GREEN instance running"
+	SITE_NAME_RUNNING=${SITE_NAME_GREEN}
 	INSTANCES_RUNNING=$[${INSTANCES_RUNNING} + 1]
 else
 	echo "GREEN instance NOT running"
-	SITE_NAME=${SITE_NAME_GREEN}
+	SITE_NAME_NOT_RUNNING=${SITE_NAME_GREEN}
 fi
 
 if echo "${SITES}" | grep -Eq "data:\s+${SITE_NAME_BLUE}\s+.*Running"
 then
 	echo "BLUE instance running"
+	SITE_NAME_RUNNING=${SITE_NAME_BLUE}
 	INSTANCES_RUNNING=$[${INSTANCES_RUNNING} + 1]
 else
 	echo "BLUE instance NOT running"
-	SITE_NAME=${SITE_NAME_BLUE}
+	SITE_NAME_NOT_RUNNING=${SITE_NAME_BLUE}
 fi
 
 if [ ${INSTANCES_RUNNING} -eq 0 ]
 then
 	echo "No instance running, default to GREEN"
-	SITE_NAME=${SITE_NAME_GREEN}
+	SITE_NAME_NOT_RUNNING=${SITE_NAME_GREEN}
+	SITE_NAME_RUNNING=${SITE_NAME_BLUE}
 fi
 
 if [ ${INSTANCES_RUNNING} -eq 2 ]
@@ -49,32 +52,50 @@ then
 	exit 1
 fi
 
-echo "Creating Azure site: ${SITE_NAME}"
-azure site create --location "West Europe" --git ${SITE_NAME} || { echo "Failed, aborting." >&2; exit 1; }
+echo "Instance running: ${SITE_NAME_RUNNING}"
+echo "Instance NOT running: ${SITE_NAME_NOT_RUNNING}"
+
+if echo "${SITES}" | grep -Eq "data:\s+${SITE_NAME_NOT_RUNNING}\s+.*Stopped"
+then
+	echo "Deleting non running Azure site: ${SITE_NAME_NOT_RUNNING}"
+	azure site delete -q ${SITE_NAME_NOT_RUNNING} || { echo "Failed, aborting." >&2; exit 1; }
+fi
+
+echo "Creating Azure site: ${SITE_NAME_NOT_RUNNING}"
+azure site create --location "West Europe" --git ${SITE_NAME_NOT_RUNNING} || { echo "Failed, aborting." >&2; exit 1; }
 
 echo "Disabling PHP"
-azure site set --php-version off ${SITE_NAME} || { echo "Failed, aborting." >&2; exit 1; }
+azure site set --php-version off ${SITE_NAME_NOT_RUNNING} || { echo "Failed, aborting." >&2; exit 1; }
 
 # echo "Enabling Web Sockets"
-# azure site set -w ${SITE_NAME} || { echo "Failed, aborting." >&2; exit 1; }
+# azure site set -w ${SITE_NAME_NOT_RUNNING} || { echo "Failed, aborting." >&2; exit 1; }
 
 echo "Getting Azure site data"
-SITE_DATA=`azure site show -d ${SITE_NAME}` || { echo "Failed, aborting." >&2; exit 1; }
+SITE_DATA=`azure site show -d ${SITE_NAME_NOT_RUNNING}` || { echo "Failed, aborting." >&2; exit 1; }
 SITE_USERNAME=`echo "${SITE_DATA}" | grep -Eo "Config publishingUserName .+$" | cut -d " " -f 3`
 SITE_PASSWORD=`echo "${SITE_DATA}" | grep -Eo "Config publishingPassword .+$" | cut -d " " -f 3`
 
 echo "Publishing Username: ${SITE_USERNAME}"
-echo "Publishing Password: ${SITE_PASSWORD}"
+echo "Publishing Password: ${SITE_PASSWORD:0:10}[...]"
 
 REMOTE_URL="https://${SITE_USERNAME}:${SITE_PASSWORD}@`git ls-remote --get-url azure | cut -d @ -f 2`"
 echo "Changing 'azure' remote url to: ${REMOTE_URL}"
-git remote set-url azure "${REMOTE_URL}" || { echo "Failed, aborting." >&2; exit 1; }
+git remote set-url azure "${REMOTE_URL}" | sed "s/${SITE_PASSWORD}/PASS/g" || { echo "Failed, aborting." >&2; exit 1; }
 
 echo "Pushing to 'azure' remote"
-git push azure master || { echo "Failed, aborting." >&2; exit 1; }
+git push azure master | sed "s/${SITE_PASSWORD}/PASS/g" || { echo "Failed, aborting." >&2; exit 1; }
 
 echo "Deleting 'azure' remote"
-git remote remove azure || { echo "Failed, aborting." >&2; exit 1; }
+git remote remove azure | sed "s/${SITE_PASSWORD}/PASS/g" || { echo "Failed, aborting." >&2; exit 1; }
+
+echo "Listing Azure sites"
+SITES=`azure site list` || { echo "Failed, aborting." >&2; exit 1; }
+
+if echo "${SITES}" | grep -Eq "data:\s+${SITE_NAME_RUNNING}\s+.*Running"
+then
+	echo "Stopping old running Azure site: ${SITE_NAME_RUNNING}"
+	azure site stop ${SITE_NAME_RUNNING} || { echo "Failed, aborting." >&2; exit 1; }
+fi
 
 echo "Clearing imported Azure account"
 azure account clear || { echo "Failed, aborting." >&2; exit 1; }
