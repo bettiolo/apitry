0b19c10f1f2623e1314654713ad13fbb0d7dc635
Marco Bettiolo <marco.bettiolo@7digital.com>
2014-03-25 18:30:27 +0000

    updated vagrant create script to support local packer

--- vagrant-create-update-box.sh
+++ vagrant-create-update-box.sh
@@ -1,20 +1,45 @@
-#!/bin/sh
+#!/bin/bash
+
+die () {
+    local message=$1
+    [ -z "$message" ] && message="Failed, aborting..."
+    echo "${BASH_SOURCE[1]}: line ${BASH_LINENO[0]}: ${FUNCNAME[1]}: $message" >&2
+    exit 1
+}
 
 if [ ! -d "packer/" ]; then
 	echo "Creating packer/ folder"
-	mkdir packer/ || { echo "Failed, aborting." >&2 ; exit 1; }
+	mkdir packer/ || die
 fi
+
 echo "Changing to packer/"
-cd packer/ || { echo "Failed, aborting." >&2 ; exit 1; }
-if [ ! -d "./packer-arch/" ]; then
+cd packer/ || die
+
+if [ ! -d "packer-arch/" ]; then
 	echo "Cloning packer-arch"
-	git clone https://github.com/elasticdog/packer-arch.git || { echo "Failed, aborting." >&2 ; exit 1; }
+	git clone https://github.com/elasticdog/packer-arch.git || die
 fi
+
 echo "Changing to packer-arch/"
-cd packer-arch/ || { echo "Failed, aborting." >&2 ; exit 1; }
+cd packer-arch/ || die
+
+if command -v ../packer >/dev/null 2>&1
+then
+	PACKER_CMD="../packer"
+fi
+
+if command -v packer >/dev/null 2>&1
+then
+	PACKER_CMD="packer"
+fi
+
+[[ -z "$PACKER_CMD" ]] && die "Packer not found"
+
 echo "Pulling packer-arch"
-git pull || { echo "Failed, aborting." >&2 ; exit 1; }
+git pull || die
+
 echo "Building virtualbox from arch-template.json"
-packer build -force -only=virtualbox-iso arch-template.json || { echo "Failed, aborting." >&2 ; exit 1; }
+../packer build -force -only=virtualbox-iso arch-template.json || die
+
 echo "Adding 'arch' vagrant box"
-vagrant box add arch packer_arch_virtualbox.box --force || { echo "Failed, aborting." >&2 ; exit 1; }
+vagrant box add arch packer_arch_virtualbox.box --force || die
