ad31c33daed067d9b038bd97b60408f4824f7ca0
Marco Bettiolo <marco@bettiolo.it>
2014-05-06 05:40:11 +0100

    Memory was calculated in KB instead of MB [skip ci]

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ var server = http.createServer(function (req, res) {
 	res.write('OS Arch: ' + os.arch() + '\n');
 	res.write('OS Release: ' + os.release() + '\n');
 	res.write('OS Uptime: ' + os.uptime() + '\n');
-	res.write('OS Free memory: ' + os.freemem() / 1024 + 'mb\n');
-	res.write('OS Total memory: ' + os.totalmem() / 1024 + 'mb\n');
+	res.write('OS Free memory: ' + os.freemem() / 1024 / 1024 + 'mb\n');
+	res.write('OS Total memory: ' + os.totalmem() / 1024 / 1024 + 'mb\n');
 	res.write('OS CPU count: ' + os.cpus().length + '\n');
 	res.write('OS CPU model: ' + os.cpus()[0].model + '\n');
 	res.write('OS CPU speed: ' + os.cpus()[0].speed + 'mhz\n');
