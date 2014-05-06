bed0827c39bbc54dca09b6712297b4118484d1ac
Marco Bettiolo <marco@bettiolo.it>
2014-05-06 05:37:49 +0100

    Added more info to hello world [skip ci]

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
 var http = require('http');
+var os = require('os')
 var port = process.env.PORT || process.env.port || 8000;
+var env = process.env.NODE_ENV || 'unknown';
 var server = http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	res.write('Hello World\n');
+	res.write('Env: ' + env + '\n');
+	res.write('Host: ' + os.hostname() + '\n');
+	res.write('OS Type: ' + os.type() + '\n');
+	res.write('OS Platform: ' + os.platform() + '\n');
+	res.write('OS Arch: ' + os.arch() + '\n');
+	res.write('OS Release: ' + os.release() + '\n');
+	res.write('OS Uptime: ' + os.uptime() + '\n');
+	res.write('OS Free memory: ' + os.freemem() / 1024 + 'mb\n');
+	res.write('OS Total memory: ' + os.totalmem() / 1024 + 'mb\n');
+	res.write('OS CPU count: ' + os.cpus().length + '\n');
+	res.write('OS CPU model: ' + os.cpus()[0].model + '\n');
+	res.write('OS CPU speed: ' + os.cpus()[0].speed + 'mhz\n');
+	res.end('\n');
 })
 server.listen(port);
 console.log('Server running on port ' + port);
