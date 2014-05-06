2876b0d25945c8502fea77cdb7c0931360d5755f
Marco Bettiolo <marco@bettiolo.it>
2014-05-06 05:08:16 +0100

    The run script should run only tests as systemd is handling node server [skip ci]

--- src/run.sh
+++ src/run.sh
@@ -7,7 +7,7 @@ die () {
     exit 1
 }
 
-echo "Running app..."
+echo "Running tests..."
 
 echo "Git version"
 git --version || die
@@ -18,11 +18,23 @@ node --version || die
 echo "Npm version"
 npm --version || die
 
+echo "Changing to /srv/http/apitry"
+cd /srv/http/apitry/
+
+echo "Stopping apitry service"
+systemctl stop apitry.service || die
+
 echo "Updating from git"
 git pull || die
 
+echo "Changing to /srv/http/apitry/src"
+cd ./src/ || die
+
 echo "Installing npm dependencies"
 npm install || die
+
+echo "Start apitry service"
+systemctl start apitry.service || die
 
 echo "Running tests"
 npm test || die
